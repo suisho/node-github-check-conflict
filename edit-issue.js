be5a797470ba005f49c2bffef89f220748a7a8ba
suisho <suisho000000@gmail.com>
2013-02-11 07:42:26 +0900

    Add labeling script

---
+++ edit-issue.js
@@ -0,0 +1,28 @@
+var util = require("util");
+// replace single repo
+module.exports = function(github, issue, editFunc, callback){
+  github.issues.getRepoIssue(issue, function(err, repo){
+  	if(err){
+       callback(err, repo);
+       return;
+  	}
+    var body = repo.body;
+    var editedRepo = editFunc(repo);
+    var editParam = {
+      user   : issue.user,
+      repo   : issue.repo,
+      number : issue.number,
+      title     : editedRepo.title,
+      body      : editedRepo.body,
+      assignee  : editedRepo.assignee,
+      state     : editedRepo.state,
+      milestone : editedRepo.milestone,
+      labels    : editedRepo.labels
+    }
+    
+    github.issues.edit(editParam, function(err, result){
+      callback(err, repo);
+    })
+  })
+}
+
---
+++ labeling.js
@@ -0,0 +1,45 @@
+var conflict = require("./check-conflict");
+var edit = require("./edit-issue");
+var async = require("async");
+
+var addLabel = function(github, issue, label, callback){
+  var func = function(repo){
+    repo.label.push(label);
+  }
+  edit(github, issue, func, callback);
+}
+
+
+module.exports = function(githib, option){
+  var user = option.user;
+  var repo = option.repo;
+  var conflictLabel = option.conflictLabel;
+  var nonConflictLabel = option.nonConflictLabel;
+  conflict(github, user, repo, function(err, result)){
+    var issueNums = Object.keys(result);
+    var tasks = [];
+    issueNums.forEach(function(num){
+      tasks.push(function(next){
+        var issue = {
+          user : user,
+          repo : repo,
+          num : num
+        };
+        var label;
+        if(result[num] && conflictLabel){
+          label = conflictLabel
+        }else if(nonConflictLabel){
+          label = nonConflictLabel
+        }
+        if(label){
+          addLabel(github, issue, label, function(err){
+            next(err)
+          })
+        }
+      })
+    })
+    async.series(tasks, function(){
+      console.log("Finished");
+    });
+  })
+})

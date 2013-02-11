4d94839c45f6e763502586de819f370c5c148a24
suisho <suisho000000@gmail.com>
2013-02-12 01:14:37 +0900

    move to lib

---
+++ index.js
@@ -0,0 +1,24 @@
+var GithubApi = require("github");
+module.exports = function(option){
+  var github = new GithubApi(
+    "version" : "3.0.0",
+    "url" : option.url,
+  )
+  if(option.token){
+    github.authorize({
+      type : "oauth",
+      token : option.token
+    })
+  }
+  return {
+    check : function(user, repo, callback){
+      require("./lib/check-conflict")(github, user, repo, callback)
+    },
+    label : function(user, repo, label, callback){
+      var option={
+        conflictLabel : label
+      }
+      require("./lib/labeling")(github, user, repo, option, callback)
+    }
+  }
+}
